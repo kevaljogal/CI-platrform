79878f7950ad28ba9c02f3c9b935333b5f5a712f
kevaljogal25 <kevaljogal25@gmail.com>
2023-02-13 18:18:16 +0530

    assignment 1 and 2 of dotnet complete

---
+++ MVC/CI-plateform-sol/CI-plateform/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics;
+using CI_plateform.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CI_plateform.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult forgot()
+        {
+            return View();
+        }
+
+        public IActionResult register()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
