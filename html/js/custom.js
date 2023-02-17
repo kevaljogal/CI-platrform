1877901d8d4c5b458db822980427f80aaa4bd10a
kevaljogal25 <kevaljogal25@gmail.com>
2023-02-17 15:31:24 +0530

    working on home page

--- html/js/custom.js
+++ html/js/custom.js
@@ -8,3 +8,44 @@ function myFunction() {
 
     }
   }
+
+
+  var wrapper = document.getElementById("wrapper");
+
+document.addEventListener("click", function (event) {
+  if (!event.target.matches(".list")) return;
+
+  // List view
+  event.preventDefault();
+  wrapper.classList.add("list");
+});
+
+document.addEventListener("click", function (event) {
+  if (!event.target.matches(".grid")) return;
+
+  // List view
+  event.preventDefault();
+  wrapper.classList.remove("list");
+});
+
+
+
+// Get the elements with class="column"
+var elements = document.getElementsByClassName("card");
+
+// Declare a loop variable
+var i;
+
+// List View
+function listView() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.width = "50%";
+  }
+}
+
+// Grid View
+function gridView() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.width = "30%";
+  }
+}
